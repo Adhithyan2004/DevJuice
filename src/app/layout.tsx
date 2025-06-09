674f4fce96a68edf37dca6e30804d2af64084a84
Adhithyan2004 <adhibanugopan@gmail.com>
2025-06-09 17:19:57 +0530

    seo optimisation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,35 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
-
+// in layout.tsx or Head.tsx
 export const metadata = {
-  title: 'DevJuice',
-  description: 'A platform for frontend developers to share and discover tools',
-  icons: {
-    icon: '/favicon.ico',
+  title: 'DevJuice -  Dev Tools Hub',
+  description:
+    'A no-login toolbox for developers. Free, fast, and open source tools for color, UI, and more.',
+  keywords:
+    'developer tools, webdev, frontend tools, color palette, css generator, open source tools,react libraries, contribution',
+  openGraph: {
+    title: 'DevJuice – Free Developer Tools',
+    description: 'Open source tools for frontend devs. Fast. No login. Just results.',
+    url: 'https://dev-juice.vercel.app',
+    siteName: 'DevJuice',
+    images: [
+      {
+        url: 'https://dev-juice.vercel.app/og.png', // must be an absolute URL
+        width: 1200,
+        height: 630,
+        alt: 'DevJuice - Developer Tools Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DevJuice - Free Dev Tools',
+    description: 'A fast, no-login toolbox for developers. Color tools, regex tester, meta viewer and more.',
+    images: ['https://dev-juice.vercel.app/og.png'],
   },
 };
 
