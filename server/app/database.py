7ba332b7a2bd5e18c6abcf8499c3662e02109ce8
Ganapathi <gana301104@gmail.com>
2025-07-02 19:46:51 +0530

    added environment setup for unix based systems

--- server/app/database.py
+++ server/app/database.py
@@ -19,6 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
